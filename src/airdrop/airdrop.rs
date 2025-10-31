161341d97b1fdb3f8152ff8a08e7e9d523ee09f1
solarpx <solarpx@protonmail.com>
2025-10-31 16:02:46 -0500

    feat: update airdrop function

--- src/airdrop/airdrop.rs
+++ src/airdrop/airdrop.rs
@@ -11,13 +11,8 @@ pub fn airdrop(rpc_url: &str, keypair_path: String, amount: f64) -> eyre::Result
     let pubkey = keypair.pubkey();
 
     // Check current balance
-    let balance = rpc_client.get_balance(&pubkey)?;
     println!("\nRequesting airdrop:");
     println!("  Account:         {}", pubkey);
-    println!(
-        "  Current Balance: {} SOL",
-        balance as f64 / 1_000_000_000.0
-    );
     println!("  Airdrop Amount:  {} SOL", amount);
 
     // Request airdrop
